49702c49fe50ead1762386a5737dd82b52b61dcc
Meiki <mickey.0103.eiking@outlook.jp>
2024-01-12 14:35:52 +0900

    Create home.php

---
+++ src/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ホーム</title>
+</head>
+<body>
+<form method="post">
+    <!-- ボタンの作成 -->
+    <button type="submit" name="home">ホーム</button>
+    <button type="submit" name="registration">登録</button>
+</form>
+</body>
+</html>
