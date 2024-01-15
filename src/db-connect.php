4eba1b56a990791b60077375250f35c6de4f73a9
Meiki <mickey.0103.eiking@outlook.jp>
2024-01-15 11:48:30 +0900

    a

---
+++ src/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1516891-final';
+    const USER = 'LAA1516891';
+    const PASS = 'Pass0103';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
--- src/home.php
+++ src/home.php
@@ -1,3 +1,4 @@
+<?php require 'db-connect.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +11,34 @@
     <!-- ボタンの作成 -->
     <button type="submit" name="home">ホーム</button>
     <button type="submit" name="registration">登録</button>
+    <a href="detail.php">
+        <img src="../image/newjeans-attention.png" alt="NO image!">
+    </a>
+    <?php
+try {
+    // データベースに接続
+    $pdo = new PDO($connect, USER, PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // IDが2の画像をデータベースから取得
+    $stmt = $pdo->prepare("SELECT image_path FROM music WHERE id = ?");
+    $imageId = 2; // ここを指定のIDに変更
+    $stmt->bindParam(1, $imageId);
+    $stmt->execute();
+
+    $imageData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($imageData) {
+        $imagePath = $imageData['image_path'];
+        echo "<a href='$imagePath' target='_blank'><img src='$imagePath' alt='画像'></a>";
+    } else {
+        echo "指定されたIDの画像が見つかりませんでした。";
+    }
+} catch (PDOException $e) {
+    echo "エラー: " . $e->getMessage();
+}
+?>
+
 </form>
 </body>
 </html>
